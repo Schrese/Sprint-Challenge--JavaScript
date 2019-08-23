7175109ce94c06713efde7b861ca2a25737f06ba
Schrese <53787633+Schrese@users.noreply.github.com>
2019-08-23 12:34:07 -0400

    finished objects-arrays.js

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -14,21 +14,48 @@
 
 // Using your dinosaur objects, log answers to these questions:
 
+const tyrannosaurus = {
+  name: 'Tyty',
+  diet: 'Carnivorous',
+  weight: '7000kg',
+  length: '12m',
+  period: 'Late Cretaceous',
+  says: function() {
+    return "RAWERSRARARWERSARARARRRR!";
+  }
+}
+
+const stegosaurus = {
+  name: 'Steggy',
+  diet: 'Herbivorous',
+  weight: '2000kg', 
+  length: '9m',
+  period: 'Late Jurassic'
+}
+
+const velociraptor = {
+  name: 'Velcro',
+  diet: 'Carnivorous',
+  weight: '15kg',
+  length: '1.8m',
+  period: 'Late Cretaceous'
+}
+
 // How much did tyrannosaurus weigh?
-console.log();
+console.log(tyrannosaurus.weight);
 
 // What was the diet of a velociraptor?
-console.log();
+console.log(velociraptor.diet);
 
 // How long was a stegosaurus?
-console.log();
+console.log(stegosaurus.length);
 
 // What time period did tyrannosaurus live in?
-console.log();
+console.log(tyrannosaurus.period);
 
 
 // Create a new roar method for the tyrannosaurus.  When called, return "RAWERSRARARWERSARARARRRR!" Log the result.
-console.log();
+console.log(tyrannosaurus.says());
 
 
 // ==== Arrays ====
@@ -52,7 +79,11 @@ const graduates = [
 
 Once you have the new array created, sort the universities alphabetically and log the result. */
 const universities = [];
-console.log(universities);
+  for (let i = 0; i < graduates.length; i++) {
+    universities.push(graduates[i].university);
+  }
+
+console.log(universities.sort());
 
 /* Request 2: Create a new array called contactInfo that contains both first name and email of each student. This will be an array of strings.
 
@@ -61,12 +92,24 @@ The resulting contact information strings should have a space between the first 
 
 Log the result of your new array. */
 const contactInfo = [];
+graduates.forEach (function(items) {
+  return contactInfo.push(`${items.first_name} ${items.email}`);
+});
 console.log(contactInfo);
 
 /* Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array called unisWithUni that contains them all. This will be an array of objects. Log the result. */
 const unisWithUni = [];
-console.log(unisWithUni);
+  for (let i = 0; i < graduates.length; i++) {
+    if (graduates[i].university === "Uni") {
+      unisWithUni.push(graduates[i]);
+    }
+    
+}
+console.log(JSON.stringify(unisWithUni));
 
+
+// if (universities.filter(universities => 'Uni')) 
+// unisWithUni.push(`${graduates[i].university}`);
 
 // ==== ADVANCED Array Methods ====
 
@@ -91,6 +134,9 @@ The zoos want to display both the scientific name and the animal name in front o
 
 */
 const displayNames = [];
+zooAnimals.forEach(function(items){
+  return displayNames.push(`Name: ${items.animal_name}, Scientific: ${items.scientific_name}`);
+});
 console.log(displayNames);
 
 /* Request 2: .map()
@@ -100,7 +146,10 @@ The zoos need a list of all their animal's names (animal_name only) converted to
 */
 
 const lowCaseAnimalNames = [];
-console.log(lowCaseAnimalNames);
+const lowCase = zooAnimals.map(function(item){
+  return item.animal_name.toLowerCase();
+})
+console.log(lowCase);
 
 /* Request 3: .filter() 
 
@@ -108,14 +157,20 @@ The zoos are concerned about animals with a lower population count. Using filter
 
 */
 const lowPopulationAnimals = [];
-console.log(lowPopulationAnimals);
+const popFilter = zooAnimals.filter(function(item){
+  return item.population < 5;
+})
+console.log(popFilter);
 
 /* Request 4: .reduce() 
 
 The zoos need to know their total animal population across the United States. Find the total population from all the zoos using the .reduce() method. Remember the reduce method takes two arguments: a callback (which itself takes two args), and an initial value for the count.
 
 */
-const populationTotal = 0;
+const populationTotal = 
+zooAnimals.reduce(function(accumulator, item){
+  return accumulator + item.population;
+},0);
 console.log(populationTotal);
 
 
